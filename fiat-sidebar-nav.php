d18c329198f1c38c01ee9bfdc0e4422cf195e9fe
benzumdahl <benzumdahl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-01 14:41:55 +0000

    Adding first version of plugin
    git-svn-id: https://plugins.svn.wordpress.org/fiat-sidebar-nav/trunk@797271 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fiat-sidebar-nav.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Plugin Name: Fiat Sidebar Nav
+ * Plugin URI: http://fiatinsight.com
+ * Description: A plugin that adds a sidebar nav
+ * Version: 1.0
+ * Author: Ben Zumdahl
+ * Author URI: http://fiatinsight.com
+ */
+
+	if(!function_exists('get_post_top_ancestor_id')){
+		function get_post_top_ancestor_id(){
+    	global $post;
+    
+    if($post->post_parent){
+        $ancestors = array_reverse(get_post_ancestors($post->ID));
+        return $ancestors[0];
+    }
+    
+    return $post->ID;
+	}}
+
+ 
+ class Fiat_Sidebar_Nav extends WP_Widget {
+    function Fiat_Sidebar_Nav() {
+        $widget_ops = array( 'classname' => 'widget_fiat_sidebar_nav', 'description' => __( "Fiat Sidebar Nav" ) );
+        $this->WP_Widget('my_avatar', __('Fiat Sidebar Nav'), $widget_ops);
+    }
+
+   function widget( $args, $instance ) {
+        extract($args);
+        $title = apply_filters( 'widget_title', empty($instance['title']) ? '' : $instance['title'], $instance, $this->id_base);
+        echo $before_widget;
+        if ( !empty( $title ) ) { echo $before_title . $title . $after_title; } ?>
+           	<ul class="page-list">
+				<?php wp_list_pages( array('title_li'=>'','depth'=>1,'child_of'=>get_post_top_ancestor_id()) ); ?>
+			</ul>	
+        <?php
+        echo $after_widget;
+    }
+
+    function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']);
+        return $instance;
+    }
+
+    function form( $instance ) {
+        $instance = wp_parse_args( (array) $instance, array( 'title' => '') );
+        $title = strip_tags($instance['title']);
+
+?>
+
+	
+
+<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+
+<?php
+
+}
+
+}
+
+function lbi_widgets_init() {
+    register_widget( 'Fiat_Sidebar_Nav' );
+}
+
+add_action( 'widgets_init', 'lbi_widgets_init' );
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Fiat Sidebar Nav ===
+Contributors: benzumdahl
+Donate link: http://fiatinsight.com/
+Tags: sidebar nav, navigation, menu, side nav
+Requires at least: 3.0
+Tested up to: 3.7.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a widget that will automatically display a list of the page’s sibling pages (the other child pages under the same parent page).
+
+== Description ==
+
+Adds a widget that will automatically display a list of the page’s sibling pages (the other child pages under the same parent page). Edit the style of the list using the following classes: page-list, page_item, current_page_item
+
+For more information please visit http://fiatinsight.com
+
+== Installation ==
+
+1. Upload this plugin folder to the `/wp-content/plugins` folder of your WordPress installation.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Drag the widget into the sidebar
+1. Title the widget, if desired
+1. Style the sidebar nav list using the classes page-list, page_item, current_page_item
+
+For more information please visit http://fiatinsight.com
+
+== Frequently Asked Questions ==
+
+= How do I change the look or style of the list? =
+
+Add CSS for these classes: page-list, page_item, current_page_item
+
+== Screenshots ==
+
+1. This is the sidebar navigation widget in action (with stylization applied)
+
+== Changelog ==
+
+= 1.0 =
+Initial release.
